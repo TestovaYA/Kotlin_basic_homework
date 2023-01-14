0262bd5f51a06f54b1867b1b810f2444f3050b20
Yuliya Testova <yuliya.testova@ural.rt.ru>
2023-01-14 21:45:02 +0500

    homework12 task2 review

--- src/main/kotlin/hw12/Task2.kt
+++ src/main/kotlin/hw12/Task2.kt
@@ -2,29 +2,61 @@ package hw12
 
 import java.io.File
 import java.util.*
-import kotlin.collections.HashSet
 
 const val FILE_PATH_TRANSLATIONS = "docs/hw12/translations.txt"
 
+val latinCharset = 'A'..'z'
+
+val dictionaryEnRu = HashMap<String, List<String>>()
+val dictionaryRuEn = HashMap<String, MutableList<String>>()
+
 fun main() {
-    val dictionary = HashSet<Pair<String, String>>()
-
-    File(FILE_PATH_TRANSLATIONS).forEachLine {
-        val substrings = it.split(" - ")
-        substrings[1].split(',').forEach { word -> dictionary.add(substrings[0] to word) }
-    }
-
     val scanner = Scanner(System.`in`)
     println("Введите слово для перевода: ")
     val word = scanner.nextLine()
 
-    val translations = mutableListOf<String>()
+    val translations = getTranslations(word)
 
-    dictionary.forEach { pair ->
-        if (pair.first == word) translations.add(pair.second)
-        if (pair.second == word) translations.add(pair.first)
+    if (translations.isEmpty()) println("В словаре нет перевода ")
+    else println(translations.joinToString(", "))
+}
+
+fun getTranslations(word: String): List<String> {
+    return if (word[0] in latinCharset) {
+        if (dictionaryEnRu.isEmpty()) fillDictionaryEnRu()
+        dictionaryEnRu[word] ?: mutableListOf()
+    } else {
+        if (dictionaryRuEn.isEmpty()) fillDictionaryRuEn()
+        dictionaryRuEn[word] ?: listOf()
+    }
+}
+
+fun fillDictionaryEnRu(): HashMap<String, List<String>> {
+    File(FILE_PATH_TRANSLATIONS).forEachLine {
+        val substrings = it.split(" - ")
+        val wordEn = substrings[0]
+        val wordsRu = substrings[1].split(",")
+
+        dictionaryEnRu[wordEn] = wordsRu
     }
 
-    if (translations.isEmpty()) println("Данного слова нет в словаре")
-    else println(translations.joinToString(", "))
+    return dictionaryEnRu
+}
+
+fun fillDictionaryRuEn(): HashMap<String, MutableList<String>> {
+    File(FILE_PATH_TRANSLATIONS).forEachLine {
+        val substrings = it.split(" - ")
+        val wordEn = substrings[0]
+        val wordsRu = substrings[1].split(",")
+
+        wordsRu.forEach { wordRu ->
+            if (dictionaryRuEn.containsKey(wordRu)) {
+                if (!dictionaryRuEn[wordRu]!!.contains(wordEn)) dictionaryRuEn[wordRu]!!.add(wordEn)
+            } else {
+                dictionaryRuEn[wordRu] = mutableListOf(wordEn)
+            }
+        }
+    }
+
+    return dictionaryRuEn
 }
